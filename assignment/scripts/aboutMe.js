c46ad1c82a90e56ed89001cec22751183f61800e
Maia Johnson <maiaj1306@gmail.com>
2022-08-25 13:14:57 -0500

    added adventurous variable

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -15,7 +15,7 @@
 // Refer back to the videos if you need help with this one.
     console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+    let adventurous = true;
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
 
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have

5606b08f56be5a81da89873672eeb3bd84d73cd1
Maia Johnson <maiaj1306@gmail.com>
2022-08-25 13:23:54 -0500

    created allowed pets constant

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -25,7 +25,7 @@
 // 11 - Add two pets to your `pets` variable
     pets = 4;
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+    const allowedPets = 4;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
 
